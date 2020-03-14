7c961bef1b92b206fbac1360c6263787801b22c7
scrazyboy97kkkk <61901058+scrazyboy97kkkk@users.noreply.github.com>
2020-03-14 13:40:40 +0800

    Add files via upload

---
+++ 0307.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import random
+
+n=0
+n=random.randint(1,20)
+while True:
+    
+    ans=int(input("Write Answer Pls"))
+    if ans == n :
+        print("Good")
+    if ans > n:
+        print("Too Big")
+    if ans < n:
+        print("Too Small")
+for i in range(5):
+    
+    ans=int(input("Write Answer Pls"))
+    if ans == n :
+        print("Good")
+    if ans > n:
+        print("Too Big")
+    if ans < n:
+        print("Too Small")
+
+
+
+
+
